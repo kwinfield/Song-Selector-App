a8f0e84525d12fcabf4bc39bcc04d867659e4716
Keith Winfield <keithwinfield@Keiths-Mac-Pro.rem.remichel.com>
2019-07-15 09:27:29 -0400

    Updates

---
+++ precache-manifest.cec46383f223aebbfcead97e321bcc87.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90752604e24b7acd1eac3e48757514d1",
+    "url": "/Song-Selector-App/index.html"
+  },
+  {
+    "revision": "9a9ef788929684567938",
+    "url": "/Song-Selector-App/static/js/2.af59a648.chunk.js"
+  },
+  {
+    "revision": "f09f136345c4213ca6ae",
+    "url": "/Song-Selector-App/static/js/main.eeb1fd3e.chunk.js"
+  },
+  {
+    "revision": "03e68f1a3d8892e76d4f",
+    "url": "/Song-Selector-App/static/js/runtime~main.c8cc66da.js"
+  }
+]);
